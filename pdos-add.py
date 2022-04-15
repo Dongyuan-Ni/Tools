fb2548fa6ac3779715470a2ea205dae8c8e0b536
Dongyuan-Ni <45381254+Dongyuan-Ni@users.noreply.github.com>
2022-04-15 22:22:52 +0800

    Update pdos-add.py

--- pdos-add.py
+++ pdos-add.py
@@ -1,33 +1,54 @@
 import numpy as np
 import csv
 import os
+
+num_orbits = 9
 ###########################################################
 # 处理split_dos脚本运行之后的数据（DOS1-DOSn）
 ###########################################################
 fname = os.listdir('.')
-# fname.remove('.DS_Store')
-fname.remove('pdos-add.py')
-fname.remove('test.py')
+tmp = []
+for i in fname:
+    if 'DOS' not in i:
+        tmp.append(i)
+for i in tmp:
+    fname.remove(i)
 ###########################################################
 with open(fname[0])as f:
     f_csv = csv.reader(f)
     state = list(f_csv)
-    data = np.zeros((len(state), 3))
+    #######Check whether the length is 9001#######
+    if len(state) != 9001:
+        state.remove(state[0])
+    #######Check whether the length is 9001#######
+    data = np.zeros((len(state), num_orbits))
     energy = np.zeros((len(state), 1))
+    combination = np.zeros((len(state), 2))
     for i in range(len(state)):
-        energy[i] = np.float(state[i][0].split()[0])
+        energy[i] = float(state[i][0].split()[0])
 ###########################################################
 
 ###########################################################
+num_atoms = len(fname)
+print('Total number of atoms: {}'.format(num_atoms))
 for name in fname:
+    print('Read file: {}'.format(name))
     with open(name)as f:
         f_csv = csv.reader(f)
         state = list(f_csv)
+        #######Check whether the length is 9001#######
+        if len(state) != 9001:
+            state.remove(state[0])
+        #######Check whether the length is 9001#######
         for i in range(len(state)):
-            a = [np.float(tmp) for tmp in state[i][0].split()[1:]]
+            a = [float(tmp) for tmp in state[i][0].split()[1:]]
             for j in range(len(a)):
                 data[i, j] += a[j]
 ###########################################################
 final = np.hstack((energy, data))
-np.savetxt('dump.dat', final)
-print('PDOS-Combine.....Done.')
+
+combination[:, 0] = final[:, 0]
+combination[:, 1] = np.sum(final[:, 1:], axis=1)/num_atoms
+np.savetxt('gray.dat', combination)
+print('DOS-Combine.....Done.')
+
